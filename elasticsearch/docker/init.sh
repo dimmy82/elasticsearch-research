24c416157a0da09fabf1fdcd3d378caf4c647a18
dimmy82 <chengdimmy82@yahoo.co.jp>
2023-04-21 16:05:56 +0900

    theme: data_updated_by_user

--- elasticsearch/docker/init.sh
+++ elasticsearch/docker/init.sh
@@ -4,4 +4,6 @@
 # echo 'http.cors.enabled: true' >> $ES_BASE/config/elasticsearch.yml
 # echo 'http.cors.allow-origin: "*"' >> $ES_BASE/config/elasticsearch.yml
 
+echo 'action.auto_create_index: false' >> $ES_BASE/config/elasticsearch.yml
+
 /usr/local/bin/docker-entrypoint.sh
